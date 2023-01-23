bc5e72b4b396df1c3a248da821f5b883d5166c04
AxidancE <Axidance123@gmail.com>
2023-01-24 03:14:09 +0500

    Comment

---
+++ SecondLesson/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace secondLesson
+{
+    class Program
+    {
+        static void Main ()
+        {
+            Console.WriteLine("Hello world!");
+            // Однострочный Комментарий
+            /*
+             * Многострочный комментарий
+             * Просто пример
+             */
+        }
+    }
+}
