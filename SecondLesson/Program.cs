58002f783fd2bd583d7049631526b9311ba79d8b
AxidancE <Axidance123@gmail.com>
2023-01-24 04:42:28 +0500

    Fourth lesson.

--- SecondLesson/Program.cs
+++ SecondLesson/Program.cs
@@ -6,27 +6,47 @@ namespace secondLesson
     {
         static void Main ()
         {
-            //byte number = 255;
-            //short number = 32767;
-            //int number = 123;
-            //long number = 3000000000001111111;
-            //float number = 33.1f;
-            //double number_2 = 32.1d;
-
-            string word = "Hello world";
-            char symbol = 'a';
-
-            bool InQuery= false;
-
-            Console.WriteLine (word);
-
-            int num_1 = 0, num_2 = 0, total = 0;
-
-            num_1 = Convert.ToInt32(Console.ReadLine());
-            num_2 = Convert.ToInt32(Console.ReadLine());
+            float userInput, result;
             
-            total = num_1 + num_2;
-            Console.WriteLine("Sum: " + total);
+            Console.Write("Input float number: ");
+
+            //userInput = Convert.ToDouble(Console.ReadLine());
+            userInput = float.Parse(Console.ReadLine());
+            
+            result = userInput + 10.3f;
+
+            result *= 1.5f;
+            
+            //result = userInput - 10.3f;
+            //result = userInput * 10.3f;
+            //result = userInput / 10.3f;
+            //result = userInput % 10.3f;
+            
+            //int a = 5, b = 10;
+            //int res = a % b;
+
+            Console.WriteLine("Result: " + result);
+
+            Console.WriteLine("----------");
+
+            // cw (+tab +tab) = Console.WriteLine();
+
+            //Console.WriteLine(Math.PI);
+            //Console.WriteLine(Math.E);
+            //Console.WriteLine(Math.Abs(-10));
+            //Console.WriteLine(Math.Ceiling(10.11f)); // To higher number
+            //Console.WriteLine(Math.Floor(10.11f)); // To lower number
+            //Console.WriteLine(Math.Round(10.11f)); // Just round number
+
+            //const int myInt = 1;
+
+            Console.WriteLine("Enter radius of cirlce: ");
+
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double area = Math.PI * Math.Pow(radius, 2);
+
+            Console.WriteLine("Area of circle with radius {0} equals {1}", radius, area);
+
         }
     }
 }
