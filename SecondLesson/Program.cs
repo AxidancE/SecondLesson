ee49224fda2c66c20f68fdb7f4b73deeb633fd94
AxidancE <Axidance123@gmail.com>
2023-01-26 05:36:53 +0500

    Lesson sixth.

--- SecondLesson/Program.cs
+++ SecondLesson/Program.cs
@@ -6,49 +6,16 @@ namespace secondLesson
     {
         static void Main ()
         {
-            int x = 5;
-            bool trueConst = true;
-            bool falseConst = false;
+            short userInput = Convert.ToInt16 (Console.ReadLine ());
 
-            if (x == 5 || x !=6)
+            switch (userInput)
             {
-                Console.WriteLine(trueConst);
-            }
-            else if (x != 5) 
-            {
-                Console.WriteLine(falseConst);
-            }
-
-            Console.WriteLine("----------");
-
-            Console.WriteLine("Enter name: ");
-            string role = Console.ReadLine();
-            if(role == "Admin")
-            {
-                Console.WriteLine("Enter User name: ");
-                string userName = Console.ReadLine();
-                Console.WriteLine($"Enter {userName} age: ");
-                short age = Convert.ToInt16(Console.ReadLine());
-
-                if (age <= 0 || age > 99)
-                {
-                    Console.WriteLine("Error. Wrong age!");
-                    Console.WriteLine($"Enter {userName} real age: ");
-                    age = Convert.ToInt16(Console.ReadLine());
-                }
-
-                if (age <= 0 || age > 99)
-                {
-                    Console.WriteLine("Error. Wrong age!");
-                    age = 1;
-                } else
-                {
-                    Console.WriteLine($"User's age: {age}");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Non admino.");
+                case 5: Console.WriteLine("Num is 5");
+                    break;
+                case 6: Console.WriteLine("Num is 6");
+                    break;
+                default: Console.WriteLine("I cant read number.");
+                    break;
             }
         }
     }
