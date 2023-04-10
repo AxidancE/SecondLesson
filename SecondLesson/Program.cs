e56c1bc4670bac64df3f2d922bd86c4161025756
AxidancE <Axidance123@gmail.com>
2023-04-10 16:32:06 +0500

    Fifteenth lesson.

--- SecondLesson/Program.cs
+++ SecondLesson/Program.cs
@@ -7,13 +7,12 @@ namespace Prime
     {
         static void Main() 
         {
-            Robot bot = new Robot();
-
-            bot.setValues("Bob", 800, new byte[] { 10, 10, 10 });
+            Robot bot = new Robot("Bob", 800, new byte[] { 10, 10, 10 });
             bot.printValues();
 
-            bot.setValues("Killah", 1000, new byte[] { 0, 0, 0 });
-            bot.printValues();
+            Robot killer = new Robot();
+            killer.setValues("Killah", 1000, new byte[] { 0, 0, 0 });
+            killer.printValues();
             //bot.name = "Bob";
             //bot.weight = 800;
             //bot.coordinates = new byte[] { 10, 10, 10 };
@@ -24,6 +23,7 @@ namespace Prime
             //killer.weight = 1000;
             //killer.name = "Killah";
             //killer.coordinates = new byte[] { 0, 0, 0 };
+            Robot.Print();
         }
     }
 }
--- SecondLesson/Robot.cs
+++ SecondLesson/Robot.cs
@@ -10,11 +10,24 @@ namespace SecondLesson
     {
         // Поля это переменные в классе?
 
+        private static int count;
+
         private string name;
         
         private int weight;
 
         private byte[] coordinates;
+
+        public Robot(string _name, int _weight, byte[] _coorditanes)
+        {
+            Console.WriteLine("Obj has been created.");
+            setValues(_name, _weight, _coorditanes);
+            count++;
+        }
+
+        public Robot() {
+            count++;
+        }
 
         public void setValues(string _name, int _weight, byte[] _coorditanes)
         {
@@ -27,6 +40,11 @@ namespace SecondLesson
         {
             Console.WriteLine($"{name}, weight: {weight}. Coordinates:");
             foreach (var el in coordinates) Console.WriteLine(el);
+        }
+
+        public static void Print()
+        { 
+            Console.WriteLine($"Count is: {count}");
         }
     }
 }
